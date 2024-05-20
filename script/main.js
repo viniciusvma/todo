6447625a27e5bdde9a74afe8875b3dcd105526ee
Vinicius Machado <134942588+viniciusvma@users.noreply.github.com>
2024-05-20 17:23:04 -0300

    Update main.js

--- script/main.js
+++ script/main.js
@@ -1,20 +1,20 @@
-const tarefa = document.querySelector('.input-tarefa')
-const adicionar = document.querySelector('.btn-adicionar')
-const listaTarefas = document.querySelector('.lista-tarefas')
+const tarefa = document.querySelector('.input-tarefa');
+const adicionar = document.querySelector('.btn-adicionar');
+const listaTarefas = document.querySelector('.lista-tarefas');
 
-let items = []
+let items = [];
 
 function leituraValores() {
-    let valores = tarefa.value
+    let valores = tarefa.value;
     items.push({
         input: valores,
         concluida: false
-    })
-    console.log(items)
-    tarefa.value = ''
+    });
+    console.log(items);
+    tarefa.value = '';
 
-    adicionarValores()
-    salvarNoLocalStorage()
+    adicionarValores();
+    salvarNoLocalStorage();
 }
 
 function adicionarValores() {
@@ -24,34 +24,36 @@ function adicionarValores() {
                       <img src="img/checked.png" class=alt="Ok" onclick='confirmarItem(${index})'>
                       <p>${tarefa.input}</p>
                       <img src="img/trash.png" alt="Apagar" onclick='deletarItem(${index})'>
-                      </li>`
-        listaTarefas.innerHTML += novali
-    })
+                      </li>`;
+        listaTarefas.innerHTML += novali;
+    });
 }
 
 function deletarItem(index) {
-    items.splice(index, 1)
-    adicionarValores()
-    salvarNoLocalStorage()
+    items.splice(index, 1);
+    adicionarValores();
+    salvarNoLocalStorage();
 }
 
 function confirmarItem(index) {
-    items[index].concluida = !items[index].concluida
-    adicionarValores()
-    salvarNoLocalStorage()
+    items[index].concluida = !items[index].concluida;
+    adicionarValores();
+    salvarNoLocalStorage();
 }
 
 function recarregarTela() {
-    const tarefaLocal = localStorage.getItem('lista')
-    items = JSON.parse(tarefaLocal)
-    adicionarValores()
+    const tarefaLocal = localStorage.getItem('lista');
+    if (tarefaLocal !== null) {
+        items = JSON.parse(tarefaLocal);
+    }
+    adicionarValores();
 }
 
 function salvarNoLocalStorage() {
-    localStorage.setItem('lista', JSON.stringify(items))
+    localStorage.setItem('lista', JSON.stringify(items));
 }
 
-
-document.addEventListener('DOMContentLoaded', recarregarTela);
-adicionar.addEventListener('click', leituraValores)
-
+document.addEventListener('DOMContentLoaded', function() {
+    recarregarTela();
+    adicionar.addEventListener('click', leituraValores);
+});
